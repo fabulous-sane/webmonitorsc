e2f466aa71460e966ef32667e031f496e84714a2
saneq <sanichkaqq@gmail.com>
2026-04-19 01:39:08 +0300

    schema syncs

---
+++ alembic/versions/40308cbecec1_pre_deploy_schema_sync.py
@@ -0,0 +1,25 @@
+"""pre-deploy schema sync
+
+Revision ID: 40308cbecec1
+Revises: 2ff6d9fdd337
+Create Date: 2026-04-18 21:59:13.116100
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision: str = '40308cbecec1'
+down_revision: Union[str, Sequence[str], None] = '2ff6d9fdd337'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
