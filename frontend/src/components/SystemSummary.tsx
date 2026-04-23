4e3fb2b30a0dbc369e8bde0551e062db7396dfb5
saneq <sanichkaqq@gmail.com>
2026-04-24 00:26:47 +0300

    fixed alignment final

--- frontend/src/components/SystemSummary.tsx
+++ frontend/src/components/SystemSummary.tsx
@@ -140,6 +140,7 @@ export default function SystemSummary() {
         </div>
       </div>
     </div>
+    )
       {/* RETENTION */}
       <div className="p-4 rounded-lg border">
   <div className="text-sm text-gray-500">Retention</div>
