85897a761d21b3edfae2379b790dfb96b5c903ce
saneq <sanichkaqq@gmail.com>
2026-05-07 00:26:39 +0300

    misc backend and UI fixes

--- app/read_models/system_stats.py
+++ app/read_models/system_stats.py
@@ -53,51 +53,46 @@ SELECT
         SELECT COUNT(*)
         FROM check_results cr
         JOIN sites s2 ON s2.id = cr.site_id
-        WHERE
-            s2.user_id = :user_id
-            AND cr.checked_at >= date_trunc('day', timezone('Europe/Kyiv', now()))
+        WHERE s2.user_id = :user_id
+          AND cr.checked_at >= NOW() - INTERVAL '24 hours'
     ) AS checks_24h,
 
     (
         SELECT COUNT(*)
         FROM check_results cr
         JOIN sites s2 ON s2.id = cr.site_id
-        WHERE
-            s2.user_id = :user_id
-            AND cr.ssl_warning = 'critical'
-            AND cr.checked_at >= date_trunc('day', timezone('Europe/Kyiv', now()))
+        WHERE s2.user_id = :user_id
+          AND cr.ssl_warning = 'critical'
+          AND cr.checked_at >= NOW() - INTERVAL '24 hours'
     ) AS ssl_critical_events,
 
     (
         SELECT COUNT(*)
         FROM check_results cr
         JOIN sites s2 ON s2.id = cr.site_id
-        WHERE
-            s2.user_id = :user_id
-            AND cr.ssl_warning = 'warning'
-            AND cr.checked_at >= date_trunc('day', timezone('Europe/Kyiv', now()))
+        WHERE s2.user_id = :user_id
+          AND cr.ssl_warning = 'warning'
+          AND cr.checked_at >= NOW() - INTERVAL '24 hours'
     ) AS ssl_warning_events,
 
     (
         SELECT COUNT(*)
         FROM check_results cr
         JOIN sites s2 ON s2.id = cr.site_id
-        WHERE
-            s2.user_id = :user_id
-            AND cr.ssl_valid = false
-            AND cr.checked_at >= date_trunc('day', timezone('Europe/Kyiv', now()))
+        WHERE s2.user_id = :user_id
+          AND cr.ssl_valid = false
+          AND cr.checked_at >= NOW() - INTERVAL '24 hours'
     ) AS ssl_invalid_events,
 
     (
         SELECT COUNT(*)
         FROM check_results cr
         JOIN sites s2 ON s2.id = cr.site_id
-        WHERE
-            s2.user_id = :user_id
-            AND cr.ssl_valid IS NULL
-            AND cr.ssl_warning IS NULL
-            AND s2.url NOT LIKE 'http://%'
-            AND cr.checked_at >= date_trunc('day', timezone('Europe/Kyiv', now()))
+        WHERE s2.user_id = :user_id
+          AND cr.ssl_valid IS NULL
+          AND cr.ssl_warning IS NULL
+          AND s2.url NOT LIKE 'http://%'
+          AND cr.checked_at >= NOW() - INTERVAL '24 hours'
     ) AS ssl_no_data_events
 
 FROM sites s
