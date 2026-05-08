8539bfdc34c6be21aacbf21612ee6d128bb5adfe
saneq <sanichkaqq@gmail.com>
2026-05-08 15:39:44 +0300

    misc backend and UI fixes

--- app/bot/telegram_bot.py
+++ app/bot/telegram_bot.py
@@ -186,7 +186,7 @@ async def list_sites(message: types.Message):
 
             await bot.send_message(
                 chat_id=message.chat.id,
-                text="",
+                text="Оберіть дію:",
                 reply_markup=main_menu(),
             )
 
